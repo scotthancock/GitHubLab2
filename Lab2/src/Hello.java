d28119e2ac6ed0d44760f6231f289c2de11f1e95
Ruvym <rmykhalets@Rim-Desktop>
2019-04-02 22:32:18 -0700

    Nvm my previous push deleted Julienne, sorry, I fix

--- Lab2/src/Hello.java
+++ Lab2/src/Hello.java
@@ -6,8 +6,8 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("220019151 - This line was added by Scott Hancock.");
+		System.out.println("219582195 - This line was added by Julienne Phan.");
 		System.out.println("301052804 - This line was added by Ruvym Mykhalets.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
